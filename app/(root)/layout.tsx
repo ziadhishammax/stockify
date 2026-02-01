0c4202d0f7a5fe311593c2592e286dbb19de9959
ZiadHisham1 <132110942+ZiadHisham1@users.noreply.github.com>
2026-02-01 23:32:35 +0200

    Modify session check and user object defaults
    Updated user session handling and added default values.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { FloatingAdBanner } from "@/components/FloatingAdBanner";
 import Header from "@/components/Header";
 import {auth} from "@/lib/better-auth/auth";
 import {headers} from "next/headers";
@@ -6,17 +7,18 @@ import {redirect} from "next/navigation";
 const Layout = async ({ children }: { children : React.ReactNode }) => {
     const session = await auth.api.getSession({ headers: await headers() });
 
-    if(!session?.user) redirect('/sign-in');
+    // if(session?.user) redirect('/sign-in');
 
     const user = {
-        id: session.user.id,
-        name: session.user.name,
-        email: session.user.email,
+        id: session?.user.id || 1256,
+        name: session?.user.name || 'john doe',
+        email: session?.user.email || 'johndoe@mail.com',
     }
 
     return (
         <main className="min-h-screen text-gray-400">
             <Header user={user} />
+            {/* <FloatingAdBanner /> */}
             <div className="container py-10">
                 {children}
             </div>
