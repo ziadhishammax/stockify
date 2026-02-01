94d6b27008c011201c13e42e9f0e652d72ca452b
ZiadHisham1 <132110942+ZiadHisham1@users.noreply.github.com>
2026-02-01 23:08:35 +0200

    Add FloatingAdBanner component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
+import { FloatingAdBanner } from "@/components/FloatingAdBanner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <FloatingAdBanner />
         {children}
         <Toaster />
       </body>
