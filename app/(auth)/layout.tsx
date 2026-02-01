8e4ce20a1ac49b327acd3fa75c8667a840e34335
ZiadHisham1 <132110942+ZiadHisham1@users.noreply.github.com>
2026-02-01 23:32:04 +0200

    Redirect unauthenticated users to home page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ import {redirect} from "next/navigation";
 const Layout = async ({ children }: { children : React.ReactNode }) => {
     const session = await auth.api.getSession({ headers: await headers() })
 
-    if(session?.user) redirect('/')
+    if(!session?.user) redirect('/')
 
     return (
         <main className="auth-layout">
